cae4f86fbc348cb91e50985bd4edea549abcee1c
Brian McCann <bmccann36@gmail.com>
2017-12-09 13:42:35 -0500

    working on averaging algo for shadows

---
+++ notes.md
@@ -0,0 +1,15 @@
+loop through the array if the value is between 10 & 240 use it if not disregard
+
+calculating the blue shift
+
+loop throgh whole array
+
+at each pixel assign red green and blue
+
+
+## for the shadows
+obj { blueVred, blueVgreen }
+blue - red  push into array
+ blue - green push into array
+
+ average the values in the obj
--- public/gelId.js
+++ public/gelId.js
@@ -3,8 +3,6 @@ makeSection1()
 makeSection2()
 
 
-
-
 function makeSection1() {
   let xPos = 0
   let yPos = 0
@@ -17,15 +15,36 @@ function makeSection1() {
   var canvas1 = document.getElementById('canvas-1');
   var ctx1 = canvas1.getContext('2d');
 
+  const tints = {
+    blueVred: [],
+    blueVgreen: []
+  }
 
   img.onload = function () {
     ctx1.drawImage(img, 0, 0, 300, 400);
+    let imageData = ctx1.getImageData(1, 1, 10, 10).data
+    for (let i = 0; i < imageData.length; i += 4) {
+      const red = imageData[i]
+      const green = imageData[i + 1]
+      const blue = imageData[i + 2]
+      tints.blueVred.push(blue - red)
+      tints.blueVgreen.push(blue - green)
+    }
+    const sum = tints.blueVred.reduce( (a,b) => a + b)
+    const avg = ( sum / tints.blueVred.length )
+    console.log(avg)
   };
+
+
+
+
+
 
   function setCoor(e) {
     var offset = this.getClientRects()[0];
     xPos = Math.round(e.clientX - offset.left)
     yPos = Math.round(e.clientY - offset.top)
+
     var pixel = ctx1.getImageData(xPos, yPos, 1, 1);
     var data = pixel.data;
     var rgb = 'rgb(' + data[0] + ', ' + data[1] +
@@ -35,33 +54,33 @@ function makeSection1() {
   }
 }
 
-  function makeSection2() {
-    let xPos = 0
-    let yPos = 0
-    const sect2 = document.getElementById('sect-2')
-    const swatch2 = document.getElementById('swatch-2')
-    var dash2 = document.getElementById('dash-2');
-    var img = new Image();
-    img.src = './images/eigth.jpg'
-    var canvas = document.getElementById('canvas-2');
-    var ctx = canvas.getContext('2d');
+function makeSection2() {
+  let xPos = 0
+  let yPos = 0
+  const sect2 = document.getElementById('sect-2')
+  const swatch2 = document.getElementById('swatch-2')
+  var dash2 = document.getElementById('dash-2');
+  var img = new Image();
+  img.src = './images/eigth.jpg'
+  var canvas = document.getElementById('canvas-2');
+  var ctx = canvas.getContext('2d');
 
-    img.onload = function () {
-      ctx.drawImage(img, 0, 0, 300, 400);
-    };
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, 300, 400);
+  };
 
-    sect2.addEventListener('mousedown', setCoor)
+  sect2.addEventListener('mousedown', setCoor)
 
-    function setCoor(e) {
-      var offset = this.getClientRects()[0];
-      xPos = Math.round(e.clientX - offset.left)
-      yPos = Math.round(e.clientY - offset.top)
-      var pixel = ctx.getImageData(xPos, yPos, 1, 1);
-      var data = pixel.data;
-      var rgb = 'rgb(' + data[0] + ', ' + data[1] +
-        ', ' + data[2] + ')';
-      dash2.textContent = rgb;
-      swatch2.style.background = rgb
-    }
+  function setCoor(e) {
+    var offset = this.getClientRects()[0];
+    xPos = Math.round(e.clientX - offset.left)
+    yPos = Math.round(e.clientY - offset.top)
+    var pixel = ctx.getImageData(xPos, yPos, 1, 1);
+    var data = pixel.data;
+    var rgb = 'rgb(' + data[0] + ', ' + data[1] +
+      ', ' + data[2] + ')';
+    dash2.textContent = rgb;
+    swatch2.style.background = rgb
   }
+}
 
